3a6f953c4b48db1b6983d9dc9ccd99cff596f8b3
Nelson Drozd <nelsondrozd@gmail.com>
2022-10-26 10:06:11 -0400

    Started back-end, created models, associations

--- models/index.js
+++ models/index.js
@@ -1,4 +1,25 @@
 const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 // Define associations (belongsToMany etc)
+User.hasMany(Post,{
+    foreignKey: 'user_id'
+})
+Post.belongsTo(User,{
+    foreignKey: 'user_id'
+})
+User.hasMany(Comment,{
+    foreignKey: 'user_id'
+})
+Comment.belongsTo(User,{
+    foreignKey: 'user_id'
+})
+Post.hasMany(Comment,{
+    foreignKey: 'post_id'
+})
+Comment.belongsTo(Post,{
+    foreignKey: 'post_id'
+})
 
-module.exports = {User}
+
+module.exports = {User, Post, Comment};
--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const app = express()
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create();
 const sequelize = require("./config/connection");
+const path = require('path')
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/home-routes.js'))
-app.use('/api/user',require('./controllers/api/user-routes.js'))
+app.use('/api/users',require('./controllers/api/user-routes.js'))
 
 app.listen(3001, () => {
     console.log('Server listening');
-    sequelize.sync({ force: true });
+    sequelize.sync({ force: false });
   });
