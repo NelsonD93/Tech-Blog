05e6dc4328e4b2ddc90d02addce9b489b5038953
Nelson Drozd <nelsondrozd@gmail.com>
2022-12-07 23:13:04 -0500

    logout functionality working

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,15 +1,17 @@
 // Taken from Module 14 mini-project
 
 const logout = async () => {
-    const response = await fetch('/api/user-routes/logout', {
+    const response = await fetch('/api/users/logout', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
     });
   
     if (response.ok) {
-      document.location.replace('/');
+      document.location.replace('/login');
+      console.log('It works')
     } else {
       alert(response.statusText);
+      console.log('Not working')
     }
   };
   
